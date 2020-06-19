d582b509c2883ad99651291fdf328a2f6de814bc
Fran <franciellefmorais@hotmail.com>
2020-06-18 23:18:54 -0300

    organizei os arquivos

--- Fase 2/Desafios/desafio1-1_exerc2.js
+++ Fase 2/Desafios/desafio1-1_exerc2.js
@@ -1,32 +1,3 @@
-// Cálculo de IMC
-// Crie um programa para calcular o IMC e nível de obesidade de uma pessoa.
-
-// Comece criando constantes para armazenar o nome, peso, altura e sexo de uma pessoa, por exemplo:
-
-// const nome = "Carlos";
-// const peso = 84;
-// const altura = 1.88;
-// A partir desses dados armazene em uma constante chamada imc o cálculo do índice de massa corporal definido pela fórmula abaixo:
-
-// peso / (altura * altura);
-// Baseado no valor obtido através desse cálculo exiba as seguintes mensagens:
-
-// SE o IMC maior ou igual a 30: Carlos você está acima do peso;
-// SE o IMC menor que 29.9: Carlos você não está acima do peso;
-
-const nome = 'Carlos'
-const peso = 84
-const altura = 1.88
-const imc = peso / (altura * altura)
-
-
-if (imc >= 30) {
-    console.log(`${nome}, você está acima do peso`)
-}
-else {
-    console.log(`${nome}, você não está acima do peso`) 
-}
-
 // Cálculo de aposentadoria
 // Crie um programa para calcular a aposentadoria de uma pessoa.
 
