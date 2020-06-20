3890242b197f042ee85c57eaf2aa376557a44c2c
Fran <franciellefmorais@hotmail.com>
2020-06-20 00:34:24 -0300

    alterei resolução do exercicio

--- Fase 2/Desafios/desafio1-1_exerc2.js
+++ Fase 2/Desafios/desafio1-1_exerc2.js
@@ -33,3 +33,14 @@ const nome = 'Silvana'
 const sexo = 'F'
 const idade = 48
 const contribuicao = 23
+const tempo = idade + contribuicao
+
+if (sexo === "F"){
+    if (tempo >= 85){
+        console.log(`${nome}, você pode se aposentar!`)
+    } else {
+        console.log(`${nome}, você ainda não pode se aposentar!`)
+    }
+} else {
+    console.log(`no momento estamos analisando apenas o sexo feminino`)
+}
