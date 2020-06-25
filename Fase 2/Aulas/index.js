3cf1830227a5dfac4e7646a31446572804dbd6da
Fran <franciellefmorais@hotmail.com>
2020-06-24 21:22:59 -0300

    incluí boleanos e organizei a padronização e escrita

--- Fase 2/Aulas/index.js
+++ Fase 2/Aulas/index.js
@@ -1,94 +1,105 @@
-// aula 1
+// criar um programa que calcula a average das Classs de students e envia mensagem do cálculo da average.
+
+const ClassA = [
+    {
+        name: "Mayk",
+        grade: 9.8
+    },
+    {
+        name: "Diego",
+        grade: 10
+    },
+    {
+        name: "Fulano",
+        grade: 2
+    }
+]
+
+const ClassB = [
+    {
+        name: "Clayton",
+        grade: 10
+    },
+    {
+        name: "Robson",
+        grade: 10
+    },
+    {
+        name: "Siclano",
+        grade: 0
+    },
+    {
+        name: "New student",
+        grade: 5
+    }
+
+]
+
+function calculateAverage(students) {
+    let sum = 0
+    for (let i = 0; i < students.length; i++) {
+        sum = sum + students[i].grade
+    }
+    const average = sum / students.length
+    
+    return average
+}
+
+const average1 = calculateAverage(ClassA)
+const average2 = calculateAverage(ClassB)
+
+function sendMessage(average, Class) {
+    if (average > 5) {
+        console.log(`${Class} average: ${average}. Congrats!`)
+    }
+    else {
+        console.log(`${Class} average: ${average}. Is not good!`)
+    }
+}
+
+
+function markAsFlunked(student) {
+    student.Flunked = false
+    if (student.grade < 5) {
+        student.Flunked = true
+    }
+}
+
+function sendMessageFlunked(student) {
+    if (student.Flunked) {
+        console.log(`${student.name} flunked!`)
+    }
+}
+
+function studentsFlunkeds(students) {
+    for (let student of students) {
+        markAsFlunked(student)
+        sendMessageFlunked(student)
+    }
+}
+
+sendMessage(average1, 'ClassA')
+sendMessage(average2, 'ClassB')
+
+studentsFlunkeds(ClassA)
+studentsFlunkeds(ClassB)
+
+
+// ======== OBSERVAÇÕES ======== 
 // variavel: tudo que eu guardo em mémoria do computador por um tempo, mas depois que executo ele some da memória
 // para criar a variavel de formato constante:
 // constante é uma variável que não aceita receber outros valores ao longo do tempo
-// const nome = 'Mayk boot'
-// console.log(nome)
-
-// aula 2
+// const name = 'Mayk boot'
+// console.log(name)
 // para fazer comentários como esse: "ctrl" + ";"
 // string é um texto, sinalizado com "" ou '' ou `` (sendo que esse útlimo é uma template string e pode colocar variaveis dentro)
 // exemplo: 
-// const nome = "Mayk"
-// const nome2 = 'Diego'
-// const nome3 = `Fran e ${nome}`
-// console.log(nome3)
-
+// const name = "Mayk"
+// const name2 = 'Diego'
+// const name3 = `Fran e ${name}`
+// console.log(name3)
 // para saber tipo de variavel 
-// console.log(typeof notaAluno01)
-
-
-// desafio 
-// criar um programa que calcula a média das turmas de alunos e envia mensagem do cálculo da média.
-
-const alunosdaturmaA = [
-    {
-        nome: "Mayk",
-        nota: 1.8
-        },
-    {
-        nome: "Diego",
-        nota: 10
-        },
-    {       
-        nome: "Fulano",
-        nota: 2
-    },
-    {
-        nome: "mais um aluno",
-        nota: 10
-    }
-]
-
-const alunosdaturmaB = [
-    {
-        nome: "Clayton",
-        nota: 10
-        },
-    {
-        nome: "Robson",
-        nota: 10
-        },
-    {       
-        nome: "Siclano",
-        nota: 0
-    },
-    {
-        nome: "Novo aluno",
-        nota: 5
-    }
-
-]
- 
-function calculaMedia(alunos) {
-    let soma = 0;
-    for (let i = 0; i < alunos.length; i++) {
-        soma = soma + alunos[i].nota
-    }
-    const media = soma / alunos.length
-    return media
-}
-
-const media1 = calculaMedia(alunosdaturmaA)
-const media2 = calculaMedia(alunosdaturmaB)
-
-
-function enviaMensagem(media, turma){
-    // condicional: se .... então ...., se não .....
-    // se a média for maior que 5, parabenizar a turma
-    if (media > 5) {
-        // faz uma coisa
-        console.log(`a média da turma ${turma} foi de ${media}. parabéns`)
-    }
-    else {
-        // faz outra coisa
-        console.log(`a média da turma ${turma} é menor que 5`)
-    }
-}
-
-enviaMensagem(media1, 'turmaA')
-enviaMensagem(media2, 'turmaB')
- 
+// console.log(typeof gradeAluno01)
 // objetos: coleção de propriedades, que recebe valores ou funcionalidades de alguma coisa na programação.
 // vetores ou array: agrupa os objetos em uma única variável
 // funções: reaproveitamento de códigos, fazer uma lógica num bloco de códigos e sempre poder contar com isso sem precisar reescrevê-los
@@ -96,3 +107,5 @@ enviaMensagem(media2, 'turmaB')
 // o método é quando a função está atrelada a um objeto.
 // estrutura de repetição: repete o bloco de códigos
 // let permite a alteração da variável
+// todo array recebe funções e propriedades nativas. Eu posso usar a propriedade length para saber quantos elementos eu tenho no meu array.
+// .table faz a informação ser exibida formatada 
