94d840f26c7a0c904052fa95e71b91d6f1161e86
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2020-08-09 21:12:01 -0300

    Usando dados dinâmicos

---
+++ Fase 2/3_iniciandoNoBackEnd/Aulas/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "GykTLqODQuU",
+        title: "Formulário Animado com JS puro e CSS Animation | Maik Brito",
+        duration: "57 min",
+        price: "Free"
+    },
+    {
+        id: "HN1UjzRSdBk",
+        title: "Desvendando o CSS Grid na prática | Mayk Brito",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "vqrjFnq3-uo",
+        title: "Criando Player de Áudio com Javascript | Mayk Brito",
+        duration: "60 min",
+        price: "Free"
+    },
+    {
+        id: "2alg7MQ6_sI",
+        title: "Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+        duration: "33 min",
+        price: "Free"
+    },
+    {
+        id: "2G_mWfG0DZE",
+        title: "Testes no NodeJS aplicando TDD com Jest | Diego Fernandes",
+        duration: "60 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável this no Javascript | Mayk Brito",
+        duration: "48 min",
+        price: "Free"
+    }
+]
--- Fase 2/3_iniciandoNoBackEnd/Aulas/server.js
+++ Fase 2/3_iniciandoNoBackEnd/Aulas/server.js
@@ -2,23 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
+    
 })
 
-server.get("/", function(req, res) {
-    return res.render("about")
+server.get("/", function (req, res) {
+    const about = {
+        avatar_url: "https://instagram.fplu9-1.fna.fbcdn.net/v/t51.2885-19/s150x150/80461162_455946308417592_6691935874713649152_n.jpg?_nc_ht=instagram.fplu9-1.fna.fbcdn.net&_nc_ohc=naUYnSgUQgsAX9jtzr0&oh=d72e24cea1337d5f9a3b010c0fe4995c&oe=5F595DBF",
+        name: "Fran Morais",
+        role: "Aluna - Rocketseat",
+        description: 'Administradora do <a href="https://www.doarei.com.br" target="_blank">Site Doarei</a>. Analista Administrativo no <a href="https://www.mg.senac.br" target="_blank">Senac Minas</a>. Aprendendo programação com a <a href="https://www.rocketseat.com.br" target="_blank">Rocketseat</a>.',
+        links: [
+            { name: "GitHub", url: "https://github.com/imFranMorais" },
+            { name: "Instagram", url: "https://www.instagram.com/imfranmorais/?hl=pt-br" },
+            { name: "LinkedIn", url: "\\https://www.linkedin.com/in/franciellemorais/" },
+        ]
+    }
+    return res.render("about", { about })
 })
 
-server.get("/portfolio", function(req, res) {
-    return res.render("portfolio")
+server.get("/portfolio", function (req, res) {
+    return res.render("portfolio", {items: videos})
 })
 
-server.listen(5000, function() {
+server.listen(5000, function () {
     console.log("server is running")
 })
