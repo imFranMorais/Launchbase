c6f8b040494e0f073c8ca3a7040625b5803360c1
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2020-08-10 21:56:37 -0300

    Resolução do desafio 3.2

---
+++ Fase 2/3_iniciandoNoBackEnd/desafios/data.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        id: "starter",
+        title: "Starter | Rocketseat",
+        duration: "5 módulos",
+        price: "Gratuito"
+    },
+    {
+        id: "launchbase",
+        title: "Launchbase | Rocketseat",
+        duration: "8 semanas",
+        price: "Pago"
+    },
+    {
+        id: "gostack",
+        title: "GoStack | Rocketseat",
+        duration: "8 semanas",
+        price: "Pago"
+    }
+]
--- Fase 2/3_iniciandoNoBackEnd/desafios/server.js
+++ Fase 2/3_iniciandoNoBackEnd/desafios/server.js
@@ -2,21 +2,37 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const conteudos = require("./data")
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render("desafio3-1_about")
+    const about = {
+        name: "Rocketseat",
+        avatar_url: "https://rocketseat.gallerycdn.vsassets.io/extensions/rocketseat/rocketseatreactnative/3.0.1/1588456740326/Microsoft.VisualStudio.Services.Icons.Default",
+        role: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
+        description: "No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa.",
+        list: "Imagine você dominando as mesmas tecnologias adotadas pelos melhores times do mundo, construindo aplicações de alta performance e se destacando entre os maiores programadores: <li>Javascript</li><li>Javascript ES6+</li><li>NodeJS</li><li>ReactJS</li><li>React Native</li>",
+        links: [
+            {name: "GitHub", url: "https://github.com/rocketseat" },
+            {name: "Instagram", url: "https://www.instagram.com/rocketseat_oficial/?hl=pt-br" },
+            {name: "Facebook", url: "https://www.facebook.com/rocketseat/" }
+
+
+        ]
+    }
+    return res.render("desafio3-1_about", { about })
 })
 
 server.get("/courses", function(req, res) {
-    return res.render("desafio3-1_courses")
+    return res.render("desafio3-1_courses", {items: conteudos})
 })
 
 server.use(function(req, res) {
