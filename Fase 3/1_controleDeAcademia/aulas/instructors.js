5435811b7452416a8b22a0f43f8f4d511c5deb0d
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2020-09-28 22:08:40 -0300

    Atualizando e excluindo instrutores

--- Fase 3/1_controleDeAcademia/aulas/instructors.js
+++ Fase 3/1_controleDeAcademia/aulas/instructors.js
@@ -1,13 +1,14 @@
 const fs = require('fs')
 const data = require('./data.json')
 const { age, date } = require('./utils')
+const Intl = require('intl')
 
 // show
 exports.show = function(req,res) {
     const { id } = req.params
 
     const foundInstructor = data.instructors.find(function(instructor){
-        return instructor.id == id
+        return id == instructor.id
     })
 
     if (!foundInstructor) return res.send("Instructor not found")
@@ -78,8 +79,50 @@ exports.post = function (req, res) {
     })
 }
 
+// put
+exports.put = function(req, res) {
+    
+    const { id } = req.body
+    let index = 0
 
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+        if (id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
 
-// update
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Write error!")
+
+        return res.redirect(`/instructors/${id}`)
+        
+    })
+}
 
 // delete
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredInstructors = data.instructors.filter(function(instructor){
+        return instructor.id != id
+    })
+
+    data.instructors = filteredInstructors
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error")
+
+        return res.redirect("/instructors")
+    })
+}
