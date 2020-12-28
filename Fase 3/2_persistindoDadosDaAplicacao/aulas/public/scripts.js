1c2a2273be8fc842418d4739b85cd23e81f587b5
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2020-12-28 19:53:17 -0300

    ajustes finais

--- Fase 3/2_persistindoDadosDaAplicacao/aulas/public/scripts.js
+++ Fase 3/2_persistindoDadosDaAplicacao/aulas/public/scripts.js
@@ -1,3 +1,5 @@
+const { create } = require("../src/app/models/Member")
+
 const currentPage = location.pathname
 const menuItems = document.querySelectorAll("header .links a")
 
@@ -40,26 +42,36 @@ return pages
 
 }
 
-const pagination = document.querySelector(".pagination")
-const filter = pagination.dataset.filter
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
+function createPagination(pagination) {
 
-let elements = ""
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
 
-for (let page of pages) {
-    if(String(page).includes("...")) {
-        elements += `<span>${page}</span>`
+    let elements = ""
+
+    for (let page of pages) {
+        if(String(page).includes("...")) {
+            elements += `<span>${page}</span>`
 
 
-    } else {
-        if (filter) {
-            elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
         } else {
-            elements += `<a href="?page=${page}">${page}</a>`
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
         }
     }
+
+    pagination.innerHTML = elements
+
 }
 
-pagination.innerHTML = elements
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+    createPagination(pagination)
+}
+
