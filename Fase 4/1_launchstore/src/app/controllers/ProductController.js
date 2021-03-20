2422d5182d7f06724af0eb3bba4d57382daa8d48
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-03-20 01:45:30 -0300

    Formatando preço do produto

--- Fase 4/1_launchstore/src/app/controllers/ProductController.js
+++ Fase 4/1_launchstore/src/app/controllers/ProductController.js
@@ -1,3 +1,4 @@
+const { formatPrice } = require('../../lib/utils')
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 
@@ -40,6 +41,9 @@ module.exports = {
         const product = results.rows[0]
 
         if(!product) return res.send("Product not found!")
+
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
 
         results = await Category.all()
         const categories = results.rows
