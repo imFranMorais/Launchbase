18987e3808f42d15a821400dddb513d6452e0184
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-03-20 00:46:41 -0300

    Criando estratégia de máscara de campo

--- Fase 4/1_launchstore/public/scripts.js
+++ Fase 4/1_launchstore/public/scripts.js
@@ -1,17 +1,18 @@
-const input = document.querySelector('input[name="price"]')
-input.addEventListener("keydown", function(e) {
-    
-    setTimeout(function() {
+const Mask = {
+    apply(input, func) {
+        setTimeout(function() {
+            input.value = Mask[func](input.value)
+        }, 1)
+
+    },
+    formatBRL(value) {
         let { value } = e.target
 
         value = value.replace(/\D/g,"")
 
-        value = new Intl.NumberFormat('pt-BR', {
+        return value = new Intl.NumberFormat('pt-BR', {
             style: 'currency',
             currency: 'BRL'
         }).format(value/100)
-
-        e.target.value = value
-
-    }, 1)
-})
+    }
+}
