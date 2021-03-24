71dc0778125169c42567ef0c984b764b62b6c90f
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-03-24 15:03:17 -0300

    Refatorando gerenciador de uploads

--- Fase 4/1_launchstore/public/scripts.js
+++ Fase 4/1_launchstore/public/scripts.js
@@ -3,7 +3,6 @@ const Mask = {
         setTimeout(function() {
             input.value = Mask[func](input.value)
         }, 1)
-
     },
     formatBRL(value) {
         value = value.replace(/\D/g,"")
@@ -16,16 +15,12 @@ const Mask = {
 }
 
 const PhotosUpload = {
+    preview: document.querySelector('#photos-preview'),
     uploadLimit: 6,
     handleFileInput(event) {
         const { files: fileList } = event.target
-        const { uploadLimit } = PhotosUpload
-
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos!`)
-            event.preventDefault()
-            return
-        }
+        
+        if (PhotosUpload.hasLimit(event)) return
 
         Array.from(fileList).forEach(file => {
             const reader = new FileReader()
@@ -34,22 +29,31 @@ const PhotosUpload = {
                 const image = new Image()
                 image.src = String(reader.result)
 
-                const div = document.createElement('div')
-                div.classList.add('photo')
-
-                div.onclick = () => alert('remover foto')
-
-                div.appendChild(image)
-
-                document.querySelector('#photos-preview').appendChild(div)
-
-
-
+                const div = PhotosUpload.getContainer(image)
+                PhotosUpload.preview.appendChild(div)
             }
 
             reader.readAsDataURL(file)
         })
+    },
+    hasLimit(event) {
+        const { uploadLimit } = PhotosUpload
 
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos!`)
+            event.preventDefault()
+            return true
+        }
+        return false
+    },
+    getContainer(image) {
+        const div = document.createElement('div')
+        div.classList.add('photo')
 
+        div.onclick = () => alert('remover foto')
+
+        div.appendChild(image)
+
+        return div
     }
 }
