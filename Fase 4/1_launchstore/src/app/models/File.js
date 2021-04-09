850962dd5ac77efdea64b5f5a5f9f65e3914daa3
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-04-09 00:44:33 -0300

    Debugando com VSCode

--- Fase 4/1_launchstore/src/app/models/File.js
+++ Fase 4/1_launchstore/src/app/models/File.js
@@ -27,10 +27,10 @@ module.exports = {
             const file = result.rows[0]
 
             fs.unlinkSync(file.path)
+            
+            return db.query(`DELETE FROM files WHERE id = $1`, [id])
 
-            return db.query(`
-            DELETE FROM files WHERE id = $1
-        `, [id])
+            
 
         }catch(err) {
             console.error(err)
