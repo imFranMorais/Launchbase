90839c0887c25980d8a969d7334183041c7e8269
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-09-27 23:14:16 -0300

    Estruturando dados para o front end

--- Fase 4/1_launchstore/src/app/controllers/HomeController.js
+++ Fase 4/1_launchstore/src/app/controllers/HomeController.js
@@ -1,4 +1,5 @@
 const { formatPrice } = require('../../lib/utils')
+
 const Product = require('../models/Product')
 const File = require('../models/File')
 
@@ -8,5 +9,24 @@ module.exports = {
         const products = result.rows
 
         if (!products) return res.send("Products not found!")
+
+        async function getImage(productId) {
+            let results = await Product.files(product.id)
+            let files = results.rows
+            files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+
+            return files[0]
+        }
+
+        const productsPromise = products.map(product => {
+            product.img = await getImage(product)
+            product.oldPrice = formatPrice(product.oldPrice)
+            product.price = formatPrice(product.price)
+            return product
+        })
+
+        const lastAdded = await Promise.all(productsPromise)
+
+        return res.render("home/index", { products: lastAdded })
     }
 }
