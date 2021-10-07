a35ee489acc445c724f6c0d554b9aa9f96ee8c26
franciellefmorais@hotmail.com <franciellefmorais@hotmail.com>
2021-10-07 16:50:32 -0300

    SQL da página de busca

--- Fase 4/1_launchstore/src/app/controllers/SearchController.js
+++ Fase 4/1_launchstore/src/app/controllers/SearchController.js
@@ -4,28 +4,49 @@ const Product = require('../models/Product')
 
 module.exports = {
     async index(req, res) {
-        let result = await Product.all()
-        const products = result.rows
+        try {
 
-        if (!products) return res.send("Products not found!")
+            let results,
+                params = {}
 
-        async function getImage(productId) {
-            let results = await Product.files(productId)
-            let files = results.rows
-            files = files.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+            const { filter, category } = req.query
 
-            return files[0]
+            if (!filter) return res.redirect("/")
+
+            params.filter = filter
+
+            if (category) {
+                params.category = category
+            }
+
+            results = await Product.search(params)
+
+            async function getImage(productId) {
+                let results = await Product.files(productId)
+                const files = results.rows.map(file => `${req.protocol}://${req.headers.host}${file.path.replace("public", "")}`)
+    
+                return files[0]
+            }
+
+            const productsPromise = results.rows.map(async product => {
+                product.img = await getImage(product.id)
+                product.oldPrice = formatPrice(product.old_price)
+                product.price = formatPrice(product.price)
+                return product
+            })
+
+            const products = await Promise.all(productsPromise)
+
+            const search = {
+                term: req.query.filter,
+                total: products.length
+            }
+
+            return res.render("search/index", { products })
+
         }
-
-        const productsPromise = products.map(async product => {
-            product.img = await getImage(product.id)
-            product.oldPrice = formatPrice(product.oldPrice)
-            product.price = formatPrice(product.price)
-            return product
-        }).filter((product, index) => index > 2 ? false : true)
-
-        const lastAdded = await Promise.all(productsPromise)
-
-        return res.render("search/index", { products: lastAdded })
+        catch (err) {
+            console.error(err)
+        }
     }
 }
